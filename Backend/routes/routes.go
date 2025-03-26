7ffa720a4140b38b0c619fc5b9c8de5553e32f23
Chulinuwu <ananzxzoxd@gmail.com>
2025-03-26 12:28:15 +0700

    init everything in project

--- Backend/routes/routes.go
+++ Backend/routes/routes.go
@@ -3,18 +3,35 @@ package routes
 import (
 	"backend-go/controllers"
 	"backend-go/middlewares"
+    "os"
+    "time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
+    frontendURL := os.Getenv("FRONTEND_URL")
+    if frontendURL == "" {
+        frontendURL = "http://localhost:5173" // fallback ค่า default
+    }
 
-	auth := r.Group("/")
-	auth.Use(middlewares.AuthMiddleware())
-	auth.GET("/me", controllers.GetMe)
-	auth.GET("/users", controllers.GetUsers)
-	auth.GET("/users/:id", controllers.GetUserByID)
-	auth.DELETE("/users/:id", controllers.DeleteUser)
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{frontendURL},
+        AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+
+    r.POST("/register", controllers.Register)
+    r.POST("/login", controllers.Login)
+
+    auth := r.Group("/")
+    auth.Use(middlewares.AuthMiddleware())
+    auth.GET("/me", controllers.GetMe)
+    auth.GET("/users", controllers.GetUsers)
+    auth.GET("/users/:id", controllers.GetUserByID)
+    auth.DELETE("/users/:id", controllers.DeleteUser)
 }
