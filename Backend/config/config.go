fd1da8519bb60638c6c04763b4ae7a8fcb3ba720
MoosaTae <sadhis.tae@gmail.com>
2025-03-30 12:03:07 +0700

    update error handling in LoadEnv to log warning and proceed with defaults

--- Backend/config/config.go
+++ Backend/config/config.go
@@ -9,6 +9,7 @@ import (
 func LoadEnv() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file, proceeding with default values")
 	}
+	// Q : is it gonna stop if it fails to load .env file?
 }
